065887acb656e9d383868533141f7dc191edf213
dounia <dounia.elmerzouki24@ump.ac.ma>
2026-05-22 16:54:42 +0100

    Fix: separate Destinataire from Client and update environment

--- src/app/components/bon-livraison/bon-livraison.ts
+++ src/app/components/bon-livraison/bon-livraison.ts
@@ -68,6 +68,6 @@ export class BonLivraisonComponent {
 
 ouvrirPdf(id: number) {
   // Ouvre le PDF dans un nouvel onglet du navigateur
-  window.open(`http://localhost:8080/api/bl/pdf/${id}`, '_blank');
+  window.open(`https://karess-backend-production-6a64.up.railway.app/api/bl/pdf/${id}`, '_blank');
 }
 }
